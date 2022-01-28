8946aab3d0c48f501ada579ef4f944499844ab35
kam1nskiy <kaminskiy2704@gmail.com>
2022-01-28 20:27:55 +0200

    add configs

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,17 +1,16 @@
-
 from django.contrib import admin
 from django.http import HttpResponse, JsonResponse
-from django.urls import path, include
+from django.urls import include, path
 
 
-def check (request):
-    data = {"name" : "Vova"}
+def check(request):
+    data = {"name": "Vova"}
     # return HttpResponse('Hello')
     return JsonResponse(data)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('health-check', check),
-    path('posts/', include("posts.urls")),
+    path("admin/", admin.site.urls),
+    path("health-check", check),
+    path("posts/", include("posts.urls")),
 ]
