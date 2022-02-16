3bdac2e3b77bbed02a2737b63da32151bf8859a3
kam1nskiy <kaminskiy2704@gmail.com>
2022-02-16 11:59:00 +0200

    set up drf

--- src/posts/api.py
+++ src/posts/api.py
@@ -1,5 +1,13 @@
-from django.http import JsonResponse
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from posts.serializers import PostSerializer
 
+from .models import Post
 
-def posts():
-    return JsonResponse({"results": []})
+class PostListCreateAPI(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostRetrieveUpdateDestroyAPI(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = "id"
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,24 @@
+
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+class TimeStampMixin(models.Model):
+    """Default timestamp mixin class could be used only for the inheritance"""
+
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Post(TimeStampMixin):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    status = models.BooleanField(null=False, blank=False, default=True)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, blank=False, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
